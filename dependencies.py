0ebce586fdf3bef0cbe601938926500fe9a77acb
nozilis <n0zilis@yandex.ru>
2026-06-18 14:23:00 +0300

    feat: add decode and verify token funcs

--- dependencies.py
+++ dependencies.py
@@ -1,7 +1,46 @@
 from typing import AsyncGenerator
 from sqlalchemy.ext.asyncio import AsyncSession
 from database import async_session_maker
+from fastapi.security import OAuth2PasswordBearer
+from passlib.context import CryptContext
+from fastapi import status, HTTPException, Depends
+from jose import jwt, JWTError
+from decouple import config
+from jwt_token import ALGORITHM
+from sqlalchemy import select
+from models import User
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 async def get_db() -> AsyncGenerator[AsyncSession, None]:
     async with async_session_maker() as session:
         yield session
+
+def decode_token(token: str):
+    try:
+        payload = jwt.decode(token, config('SECRET_KEY'), algorithms=[ALGORITHM])
+        return payload
+    except JWTError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Could not validate credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
+async def get_current_user(token: str = Depends(oauth2_scheme), db: AsyncSession = Depends(get_db)):
+    payload = decode_token(token)
+    if payload is None:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Could not validate credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    username = payload.get("sub")
+    db_user = select(User).where(User.username == username)
+    db_request = await db.execute(db_user)
+    db_user = db_request.scalar_one_or_none()
+    if db_user is None:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Пользователь не найден")
+    return db_user
